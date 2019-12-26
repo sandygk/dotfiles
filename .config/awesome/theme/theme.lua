38263d3b3b9af5b64cfcf637dc2cc218c1a15cb8
Sandy <sandygk@gmail.con>
2019-12-26 15:38:19 -0500

    Update

--- .config/awesome/theme/theme.lua
+++ .config/awesome/theme/theme.lua
@@ -51,9 +51,6 @@ theme.menu_width  = dpi(100)
 theme.layout_floating  = theme_path.."images/floating.png"
 theme.layout_max = theme_path.."images/max.png"
 theme.layout_tile = theme_path.."images/tile.png"
-theme.layout_fullscreen = theme_path.."images/fullscreen.png"
-
--- Icon theme
-theme.icon_theme = nil
+theme.layout_fullscreen = theme_path.."images/max.png"
 
 return theme
